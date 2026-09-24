8cd39490998803a87d57d0a4d4fedbbc5bc515d7
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-24 16:23:24 +0000

    test: align extracted authentication specs with RuboCop conventions

--- spec/domains/auth/challenges/issue_spec.rb
+++ spec/domains/auth/challenges/issue_spec.rb
@@ -7,10 +7,11 @@ RSpec.describe Auth::Challenges::Issue do
 
   it "issues an email OTP challenge and schedules the sign-in email" do
     delivery = instance_double(ActionMailer::MessageDelivery, deliver_later: nil)
-    expect(AuthMailer).to receive(:otp).with(email: transaction.identifier, code: /\A\d{6}\z/).and_return(delivery)
+    allow(AuthMailer).to receive(:otp).and_return(delivery)
 
     challenge = described_class.call(transaction: transaction, purpose: "email_otp")
 
+    expect(AuthMailer).to have_received(:otp).with(email: transaction.identifier, code: /\A\d{6}\z/)
     expect(challenge).to be_persisted
     expect(challenge.purpose).to eq("email_otp")
     expect(challenge).not_to be_expired
@@ -18,10 +19,11 @@ RSpec.describe Auth::Challenges::Issue do
 
   it "issues a password reset challenge using the same shared workflow" do
     delivery = instance_double(ActionMailer::MessageDelivery, deliver_later: nil)
-    expect(AuthMailer).to receive(:password_reset).with(email: transaction.identifier, code: /\A\d{6}\z/).and_return(delivery)
+    allow(AuthMailer).to receive(:password_reset).and_return(delivery)
 
     challenge = described_class.call(transaction: transaction, purpose: "password_reset")
 
+    expect(AuthMailer).to have_received(:password_reset).with(email: transaction.identifier, code: /\A\d{6}\z/)
     expect(challenge.purpose).to eq("password_reset")
     expect(challenge.secret_hash).to match(/\A[0-9a-f]{64}\z/)
   end
