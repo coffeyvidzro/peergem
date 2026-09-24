bc1363e13503a54643b15c96095ba5025f357876
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-24 16:24:08 +0000

    style: correct GeoIP integration indentation

--- app/integrations/geoip/locate.rb
+++ app/integrations/geoip/locate.rb
@@ -2,14 +2,14 @@
 
 module Geoip
   class Locate
-      def self.call(ip_address:)
-        reader = Rails.application.config.x.geoip.reader
-        return unless reader && ip_address
+    def self.call(ip_address:)
+      reader = Rails.application.config.x.geoip.reader
+      return unless reader && ip_address
 
-        result = reader.city(ip_address.to_s)
-        [ result.city.name, result.country.name ].compact_blank.join(", ").presence
-      rescue StandardError
-        nil
-      end
+      result = reader.city(ip_address.to_s)
+      [ result.city.name, result.country.name ].compact_blank.join(", ").presence
+    rescue StandardError
+      nil
+    end
   end
 end
