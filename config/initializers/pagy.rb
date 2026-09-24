093ac7341269e09798b13a3ad8c6b87c12cf1028
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-24 07:24:45 +0000

    Wire application infrastructure stack

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-require "pagy/extras/limit"
-require "pagy/extras/countless"
-require "pagy/extras/array"
-require "pagy/extras/overflow"
-Pagy::DEFAULT[:overflow] = :exception
+require "pagy"
+
+# Pagy 43 ships pagination types in core and configures them through OPTIONS.
+# Freeze the global options after application defaults are assigned.
+Pagy::OPTIONS[:limit] = 20
+Pagy::OPTIONS[:client_limit] = 100
+Pagy::OPTIONS.freeze
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,30 +10,12 @@ redis_config = {
 Sidekiq.configure_server do |config|
   config.redis = redis_config
 
-  config.client_middleware do |chain|
-    chain.add SidekiqUniqueJobs::Middleware::Client
-  end
-
-  config.server_middleware do |chain|
-    chain.add SidekiqUniqueJobs::Middleware::Server
-  end
-
-  SidekiqUniqueJobs::Server.configure(config)
-
   schedule = YAML.safe_load_file(Rails.root.join("config/sidekiq_schedule.yml"), aliases: true)
   Sidekiq::Cron::Job.load_from_hash!(schedule)
 end
 
 Sidekiq.configure_client do |config|
   config.redis = redis_config
-
-  config.client_middleware do |chain|
-    chain.add SidekiqUniqueJobs::Middleware::Client
-  end
-end
-
-SidekiqUniqueJobs.configure do |config|
-  config.enabled = !Rails.env.test?
 end
 
 # Sidekiq uniqueness is not financial idempotency. Enforce payment and wallet
